4a5de6f705ffd64d0f4e9e0b16f2d594f26d5748
dfloresdev <hello@dflores.dev>
2020-03-19 13:25:32 -0600

    [SECTION] component Menu ready

---
+++ src/components/Menu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import signature from "../assets/statics/Uriel-signed.png";
+import "../assets/styles/components/Menu.css";
+
+const Menu = () => (
+  <div className="container">
+    <nav className="menu">
+      <div className="menu__signature">
+        <img src={signature} alt="Signature Uriel Ramirez" />
+      </div>
+      <div className="menu__options">
+        <a href="/">
+          <p>Work</p>
+        </a>
+        <a href="/">
+          <p>About</p>
+        </a>
+        <a href="/">
+          <p>More</p>
+        </a>
+      </div>
+      <div className="menu__button">
+        <a className="basic-button" href="/">
+          Let's Talk
+        </a>
+      </div>
+    </nav>
+  </div>
+);
+
+export default Menu;
---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../assets/statics/logo.svg";
+import Menu from "../components/Menu";
+import "../assets/styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
